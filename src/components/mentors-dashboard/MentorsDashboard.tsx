9406965e6a9c73464cb2d7d1baa5645ce214655b
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-07-15 17:16:09 +0530

    Added Mentor-Trader Association

--- src/components/mentors-dashboard/MentorsDashboard.tsx
+++ src/components/mentors-dashboard/MentorsDashboard.tsx
@@ -1,23 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { Button, Tag } from "antd";
-import { ArrowRightOutlined, CloudDownloadOutlined, UserAddOutlined } from "@ant-design/icons";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Drawer,
+  List,
+  Modal,
+  Select,
+  Tag,
+  message,
+} from "antd";
+import {
+  ArrowRightOutlined,
+  UserAddOutlined,
+  UserDeleteOutlined,
+  UsergroupAddOutlined,
+} from "@ant-design/icons";
 import { Flex } from "antd"; // Assuming you have a Flex component from Ant Design
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useMentorService from "../../hooks/useMentor";
 import { IUser } from "../../types/data";
 import { ColumnsType } from "antd/es/table";
 import { useAppSelector } from "../../redux/hooks";
 import CustomTable from "../common/table/CustomTable";
-
+import avatar from "../../assets/images/avatar.png";
 const MentorsDashboard: React.FC = () => {
-  const { getAllMentors } = useMentorService();
+  const {
+    getAllMentors,
+    getAllTradersAssigned,
+    getAllUnassignedTraders,
+    assignTrader,
+    removeTrader,
+  } = useMentorService();
   const { user } = useAppSelector((state) => {
     return state.auth;
   });
-
   const [mentors, setMentors] = useState<IUser[]>([]);
+  const [assignedTraders, setAssignedTraders] = useState<IUser[]>([]);
+  const [unAssignedTraders, setUnassignedTraders] = useState<IUser[]>([]);
   const [totalMentors, setTotalMentors] = useState(0);
+  const [showTradersDrawer, setShowTradersDrawer] = useState(false);
+  const [showUnasignedTradersDrawer, setShowUnasignedTradersDrawer] =
+    useState(false);
+  const [showRemoveUserModal, setShowRemoveUserModal] = useState(false);
+  const [showAddUserModal, setShowAddUserModal] = useState(false);
+  const [selectedMentor, setSelectedMentor] = useState<IUser | null>(null);
+  const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
+  const [mentorSelectedToAssign, setMentorSelectedToAssign] = useState<{
+    firstName: string;
+    id: string;
+  } | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isTradersLoading, setIsTradersLoading] = useState(false);
   const navigate = useNavigate();
 
   const fetchAllMentors = async () => {
@@ -34,10 +68,89 @@ const MentorsDashboard: React.FC = () => {
       setIsLoading(false);
     }
   };
+  const fetchAllTraders = async () => {
+    try {
+      setIsTradersLoading(true);
+      const res = await getAllTradersAssigned(selectedMentor?._id!);
+      if (res.status === 200) {
+        setAssignedTraders(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsTradersLoading(false);
+    }
+  };
+  const fetchAllUnassignedTraders = async () => {
+    try {
+      setIsTradersLoading(true);
+      const res = await getAllUnassignedTraders();
+      if (res.status === 200) {
+        setUnassignedTraders(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsTradersLoading(false);
+    }
+  };
+  const handleAssignTrader = async () => {
+    if (!selectedUser && !mentorSelectedToAssign) return;
+    try {
+      setIsLoading(true);
+      const res = await assignTrader(
+        selectedUser?._id!,
+        mentorSelectedToAssign?.id!
+      );
+      if (res.status === 200) {
+        message.success("Trader Assigned!");
+        fetchAllUnassignedTraders();
+        fetchAllMentors();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+      setShowAddUserModal(false);
+      setSelectedMentor(null)
+    }
+  };
+  const handleRemoveTrader = async () => {
+    if (!selectedUser) return;
+    try {
+      setIsLoading(true);
+      const res = await removeTrader(selectedUser?._id!);
+      if (res.status === 200) {
+        message.success("Trader removed!");
+        const filteredList = assignedTraders.filter((e) => {
+          return e._id != selectedUser?._id;
+        });
+        setAssignedTraders(filteredList);
+        fetchAllMentors();
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSelectedUser(null)
+      setIsLoading(false);
+      setShowRemoveUserModal(false);
+    }
+  };
 
   useEffect(() => {
     fetchAllMentors();
+    fetchAllUnassignedTraders();
   }, []);
+  useEffect(() => {
+    if (selectedMentor) {
+      fetchAllTraders();
+    }
+  }, [selectedMentor]);
+  useEffect(() => {
+    if (showUnasignedTradersDrawer) {
+      fetchAllUnassignedTraders();
+    }
+  }, [showUnasignedTradersDrawer]);
   const columns: ColumnsType<IUser> = [
     {
       title: "Mentor",
@@ -54,10 +167,23 @@ const MentorsDashboard: React.FC = () => {
     },
     {
       title: "Traders",
-      dataIndex: "traders",
-      key: "traders",
-      render:()=>{ return <Button iconPosition="end" icon={<ArrowRightOutlined/>}> 0 Traders </Button> }
-
+      dataIndex: "traderCount",
+      key: "traderCount",
+      render: (text: number, record: IUser) => {
+        return (
+          <Button
+            onClick={() => {
+              setShowTradersDrawer(true);
+              setSelectedMentor(record);
+            }}
+            iconPosition="end"
+            icon={<ArrowRightOutlined />}
+          >
+            {" "}
+            {Number(text) == 1 ? text + " Trader" : text + " Traders"}{" "}
+          </Button>
+        );
+      },
     },
 
     {
@@ -73,6 +199,123 @@ const MentorsDashboard: React.FC = () => {
 
   return (
     <div className="p-4">
+      {/* SIDE COMPONENT TO DISPLAY TRADER */}
+      <Drawer
+        open={showTradersDrawer}
+        onClose={() => {
+          setShowTradersDrawer(false);
+        }}
+      >
+        <h1 className="text-lg">
+          Traders assigned to {selectedMentor?.firstName}
+        </h1>
+        <List
+          loading={isTradersLoading}
+          dataSource={assignedTraders!}
+          renderItem={(item) => (
+            <List.Item key={item.email}>
+              <List.Item.Meta
+                avatar={<Avatar src={item.profile_url_image || avatar} />}
+                title={<Link to="/">{item.firstName}</Link>}
+                description={item.email}
+              />
+              <UserDeleteOutlined
+                onClick={() => {
+                  setShowRemoveUserModal(true);
+                  setSelectedUser(item);
+                }}
+              />
+            </List.Item>
+          )}
+        />
+        <Modal
+          open={showRemoveUserModal}
+          onCancel={() => {
+            setShowRemoveUserModal(false);
+          }}
+          confirmLoading={isLoading}
+          onOk={handleRemoveTrader}
+        >
+          <h1>
+            Remove {selectedUser?.firstName} from {selectedMentor?.firstName}'s
+            mentorship?
+          </h1>
+        </Modal>
+      </Drawer>
+      <Drawer
+        style={{ borderTopLeftRadius: "16px", borderTopRightRadius: "16px" }}
+        className=" rounded-tr-10"
+        height={"90%"}
+        placement="bottom"
+        open={showUnasignedTradersDrawer}
+        onClose={() => {
+          setShowUnasignedTradersDrawer(false);
+        }}
+      >
+        <h1 className="text-lg">Traders with no mentors assigned</h1>
+        <List
+          loading={isTradersLoading}
+          dataSource={unAssignedTraders!}
+          renderItem={(item) => (
+            <List.Item onClick={() => {
+              setShowAddUserModal(true);
+              setSelectedUser(item);
+            }} className=" cursor-pointer hover:bg-slate-100 px-3 " key={item.email}>
+              <List.Item.Meta
+                avatar={<Avatar src={item.profile_url_image || avatar} />}
+                title={<Link to="/">{ !item.firstName?"Couldn't fetch name": item.firstName+" "+item.lastName}</Link>}
+                description={item.email}
+              />
+              <UserAddOutlined
+                onClick={() => {
+                  setShowAddUserModal(true);
+                  setSelectedUser(item);
+                }}
+              />
+            </List.Item>
+          )}
+        />
+        <Modal
+          confirmLoading={isLoading}
+          open={showAddUserModal}
+          onCancel={() => {
+            setShowAddUserModal(false);
+            setSelectedUser(null);
+            setMentorSelectedToAssign(null);
+          }}
+          onOk={handleAssignTrader}
+        >
+          <h1>Select a mentor</h1>
+          <Select
+            
+            onChange={(value: string) => {
+              const selectedMentor = mentors.find(
+                (mentor) => mentor._id === value
+              );
+              if (selectedMentor) {
+                setMentorSelectedToAssign({
+                  id: selectedMentor._id,
+                  firstName: selectedMentor.firstName,
+                });
+              } else {
+                setMentorSelectedToAssign(null);
+              }
+            }}
+            style={{ width: "100%" }}
+            value={
+              mentorSelectedToAssign ? mentorSelectedToAssign.id : undefined
+            }
+          >
+            <Select.Option disabled value={undefined}>Select Mentor</Select.Option>
+            {mentors.map((mentor: IUser) => (
+              <Select.Option key={mentor._id} value={mentor._id}>
+                {mentor.firstName+" "+mentor.lastName}
+              </Select.Option>
+            ))}
+          </Select>
+       
+        </Modal>
+      </Drawer>
       <h1 className="text-2xl font-bold mb-4">All MENTORS</h1>
       <div className="border border-[#EAECF0]">
         <Flex justify="space-between" align="center" className="px-6 py-6">
@@ -89,13 +332,22 @@ const MentorsDashboard: React.FC = () => {
                 navigate("/admin/create-mentor");
               }}
               type="default"
-              icon={<UserAddOutlined />}
+              icon={<UsergroupAddOutlined />}
             >
               Create Mentor
             </Button>
-            <Button type="default" icon={<CloudDownloadOutlined />}>
-              Export
-            </Button>
+
+            <Badge color="blue" size="small" count={unAssignedTraders.length}>
+              <Button
+                onClick={() => {
+                  setShowUnasignedTradersDrawer(true);
+                }}
+                type="default"
+                icon={<UserAddOutlined />}
+              >
+                Assign Traders
+              </Button>
+            </Badge>
           </div>
         </Flex>
       </div>
