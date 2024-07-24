e3ca01e1fc10cd829f7ee421f43120091b3186d1
Akshay Bagai <Akshay.Bagai@alphait.us>
2024-07-24 10:29:17 +0530

    feat: added dark mode setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-//@ts-ignore
 import authReducer from "./slices/authSlice";
+import themeReducer from "./slices/themeSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import { combineReducers } from "@reduxjs/toolkit";
 
 const persistConfig = {
-  storage: storage,
-  key: "root",
+  storage,
+  key: 'root',
   version: 1,
 };
 
-const reducer = combineReducers({ auth: authReducer});
+const reducer = combineReducers({
+  auth: authReducer,
+  theme: themeReducer,
+});
+
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
