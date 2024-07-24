4029f2731687afbe6bb9835f3419a15cd2ccf560
Akshay Bagai <Akshay.Bagai@alphait.us>
2024-07-24 10:31:49 +0530

    fix: spacing

--- src/components/layout/custom-layout/CustomLayout.tsx
+++ src/components/layout/custom-layout/CustomLayout.tsx
@@ -95,7 +95,7 @@ const CustomLayout: React.FC<CustomLayoutProps> = ({ children }) => {
       <Sider className="bg-dark-blue" style={{ background: "#262633" }}>
         <div className="text-2xl text-white text-center mt-4 mb-8 font-bold">
           EarningEdge<span className="text-[#637CFF]">.</span>
-        </div>
+        </div> 
         <Menu
           theme="dark"
           mode="inline"
