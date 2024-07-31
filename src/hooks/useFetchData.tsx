b910add2afd5c698b8a14c5d9929a0e09e87ad15
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-07-31 12:49:41 +0530

    Fixed dates in the selector

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useCallback } from "react";
 import useAxios from "./useAxios";
 import { AxiosError } from "axios";
+import { message } from "antd";
+import { useAppDispatch } from "../redux/hooks";
+import { logout } from "../redux/slices/authSlice";
 
 interface UseFetchData<T> {
   data: T | null;
@@ -13,15 +16,22 @@ const useFetchData = <T,>(endpoint: string): UseFetchData<T> => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<AxiosError | null>(null);
-  const axiosInstance = useAxios();
+  const dispatch = useAppDispatch();
 
+  const axiosInstance = useAxios();
+ 
   const fetchData = useCallback(async () => {
     setLoading(true);
     setError(null);
     try {
       const response = await axiosInstance.get<T>(endpoint);
-      setData(response.data);
-    } catch (err) {
+        setData(response.data);
+      } catch (err: any) {
+        if(err.response?.status===401){
+          message.error("Session expired, login again");
+          dispatch(logout())
+          window.location.reload()
+        }
       setError(err as AxiosError);
     } finally {
       setLoading(false);
