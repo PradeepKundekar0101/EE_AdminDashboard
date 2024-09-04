7f70c003783e00cd8877711bd6e6b5dab8b4002a
sirdesai22 <sirdesai.work@gmail.com>
2024-09-04 12:21:32 +0530

    changed BD menu to a modal in UserProfile

--- src/components/common/avatar-details/AvatarDetails.tsx
+++ src/components/common/avatar-details/AvatarDetails.tsx
@@ -1,35 +1,72 @@
-import {   Avatar, Flex } from 'antd'
+import { Avatar, Button, Flex, Modal } from "antd";
 
 // import exportIcon from '../../../assets/images/dashboard/Icon.svg'
-import { IUser } from '../../../types/data'
+import { IUser } from "../../../types/data";
+import { useState } from "react";
+import UserSalesModal from "../../user-sales-modal/UserSalesModal";
 
 interface ProfileSectionProps {
-  user: IUser
+  user: IUser;
 }
 const ProfileSection: React.FC<ProfileSectionProps> = ({ user }) => {
   // console.log(user)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className='bg-dark-blue relative flex w-full rounded-xl p-6 dark:border dark:border-white'>
-      <Avatar size={198} src={user?.profile_image_url||"/avatar.png"} className='mx-8' />
-      <div className='mt-4 rounded-full'>
-        <Flex align='center'>
-          <h2 className=' mr-2 text-3xl text-white'>{user?.firstName} {user?.lastName}</h2>
+    <div className="bg-dark-blue relative flex w-full rounded-xl p-6 dark:border dark:border-white">
+      <Avatar
+        size={198}
+        src={user?.profile_image_url || "/avatar.png"}
+        className="mx-8"
+      />
+      <div className="mt-4 rounded-full">
+        <Flex align="center">
+          <h2 className=" mr-2 text-3xl text-white">
+            {user?.firstName} {user?.lastName}
+          </h2>
           {/* <Badge
             count={user.label}
             style={{ backgroundColor: 'white', color: 'black' }}
           /> */}
         </Flex>
 
-        <div className='mt-2 text-[#C5C5C5]'>
+        <div className="mt-2 text-[#C5C5C5]">
           {/* <p>Client ID: {user.clientId} </p> */}
           <p>Email - {user?.email}</p>
           <p>Contact - {user?.phoneNumber} </p>
           <p>Occupation: {user?.occupation || "N/A"} </p>
         </div>
       </div>
-      {/* <Button className='absolute right-5 top-5 h-10 px-4 rounded-lg' icon={<img src={exportIcon} />}>Export</Button> */}
+      <Button
+        className="absolute right-5 top-5 h-10 px-4 rounded-lg"
+        onClick={showModal}
+      >
+        Sales
+      </Button>
+      <Modal
+        title="User Sales Data"
+        centered
+        width={1000}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[]}
+      >
+        <UserSalesModal />
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSection
+export default ProfileSection;
---
+++ src/components/user-sales-modal/UserSalesModal.tsx
@@ -0,0 +1,129 @@
+
+import { Col, Row, Table } from "antd";
+// import CustomLayout from "../../components/layout/custom-layout/CustomLayout";
+// import ProfileSection from "../../components/common/avatar-details/AvatarDetails";
+import StatsBox from "../../components/common/profile-card/ProfileCard";
+
+import { useParams } from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData";
+
+
+interface SalesDataItem {
+  _id: string;
+  referralCode: string;
+  userId: string;
+  usersCount: number;
+  paidUsersCount: number;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+interface ProfileDataResponse {
+  status: string;
+  data: {
+    user: any;
+    salesData: SalesDataItem[];
+  };
+}
+
+const UserSalesModal = () => {
+  const { userId } = useParams<{ userId: string }>();
+
+  //Fetch Personal data
+  const {
+    data: userData,
+    loading,
+    error,
+  } = useFetchData<{ data: any }>(`/user/${userId}`);
+  const { data: leadsData, loading: leadsLoading, error: leadsError } = useFetchData(`/sales/getLeads/${userId}`);
+  const columns = [
+    {
+      title: 'Profile Image',
+      dataIndex: 'profile_image',
+      key: 'profile_image',
+      render: (text: string) => <img src={text} alt="Profile" style={{ width: 50, height: 50, borderRadius: '50%' }} />,
+    },
+    {
+      title: 'Full Name',
+      dataIndex: 'full_name',
+      key: 'full_name',
+    },
+    {
+      title: 'Contact Number',
+      dataIndex: 'contact_number',
+      key: 'contact_number',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+  ];
+  
+
+  //Fetch Cards Data
+  const { data: profileData } = useFetchData<ProfileDataResponse>(
+    `/sales/users/${userId}`
+  );
+  console.log("profileData = ", profileData?.data.user);
+
+
+
+  const formatValue = (value: string | number): string => {
+    const numValue = typeof value === "string" ? parseFloat(value) : value;
+    if (isNaN(numValue)) return value?.toString();
+    if (Number.isInteger(numValue)) return numValue?.toString();
+    return numValue.toFixed(2);
+  };
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    // <CustomLayout>
+      <div className="p-10 bg-slate-50 flex flex-col">
+        <Row justify={"space-between"} align={"middle"} className="mt-10">
+          <Col span={6}>
+            <StatsBox
+              title="Total Leads"
+              value={formatValue(profileData?.data?.user.usersCount)}
+              color={"black"}
+            />
+          </Col>
+          <Col span={6}>
+            <StatsBox
+              title="Total Paid Users"
+              value={formatValue(profileData?.data?.user.paidUsersCount)}
+              color={"black"}
+            />
+          </Col>
+          <Col span={6}>
+            <StatsBox
+              title="Referral Code"
+              value={formatValue(profileData?.data?.user.referralCode)}
+              color={"black"}
+            />
+          </Col>
+        </Row>
+        
+        
+        <div className="mt-10">
+          <h1>Leads</h1>
+          {leadsLoading ? (
+            <p>Loading Leads...</p>
+          ) : leadsError ? (
+            <p>Error: {leadsError.message}</p>
+          ) : (
+            //@ts-ignore
+            <Table  columns={columns} dataSource={leadsData?.data} rowKey="_id" />
+          )}
+        </div>
+        
+      </div>
+    // </CustomLayout>
+  );
+  
+};
+
+export default UserSalesModal;
